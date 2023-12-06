ce7b3d3a3f998b12524099ea2eb6320b11b87a1b
DesmondThema <kgothatso@lulaloop.co.za>
2023-12-06 20:22:41 +0200

    fix(config): remove credentials

--- config/panda.php
+++ config/panda.php
@@ -4,6 +4,4 @@ return [
     'sessions_url' => env('SESSIONS_URL', 'https://api.joinpanda.com/api/session?page_size=100&page=1&home_territory=all'),
     'keywords_url' => env('KEYWORDS_URL', 'https://api.joinpanda.com/api/me'),
     'login_url' => env('LOGIN_URL', 'https://api.joinpanda.com/api/auth/email/login/'),
-    'user_email' => env('USER_EMAIL'),
-    'user_password' => env('USER_PASSWORD'),
 ];
